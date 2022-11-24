1cabba24ca3e5699d5d70fd93e1c94628a094235
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-11-24 20:10:22 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(567, 450)
+        MainWindow.resize(597, 445)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
